6127835b9ca167116287b551249f6319176a871d
astatim <astatim@outlook.com>
2022-08-08 17:14:10 +0800

    project sunphase: connect the astral and the website

--- sunphase_astral.py
+++ sunphase_astral.py
@@ -3,28 +3,27 @@ import datetime
 from astral.sun import sun
 
 
-def city_astral():
-    # in the future, this will be moved to the url.
-    name = input('Please type the name here:')
+def city_astral(name):
 
     # put the tuple into the dictionary
     LocationInfos = {'Beijing': ("Beijing", "China", "Asia/Shanghai", 39.904211, 116.407395),
                      'Shanghai': ("Shanghai", "China", "Asia/Shanghai", 31.231707, 121.472641)}
 
-    # get the data from the dictionary
-    city = LocationInfo(LocationInfos[name])
+    # read the tuple from the dictionary and divide it
+    name, region, timezone, latitude, longitude = LocationInfos[name]
 
-    # these are the test codes of line 13
+    # initialize data to compute the sunrise time
+    city = LocationInfo(name, region, timezone, latitude, longitude)
+
+    # these are the test codes of line 18, it will be deleted in the future
     print((
-        f"Information for {city.name}/{city.region}\n"
-        f"Timezone: {city.timezone}\n"
-        f"Latitude: {city.latitude:.02f}; Longitude: {city.longitude:.02f}\n"
+        f"Information for {name}/{region}\n"
+        f"Timezone: {timezone}\n"
+        f"Latitude: {latitude:.02f}; Longitude: {longitude:.02f}\n"
     ))
 
     # this will print the sunrise time
-    s = sun(city.observer, date=datetime.date(2022, 8, 7), tzinfo='Asia/Shanghai')
+    s = sun(city.observer, date=datetime.date(2022, 8, 7), tzinfo=timezone)
     print(f'Sunrise: {s["sunrise"]}\n')
 
-
-if __name__ == '__main__':
-    city_astral()
+    return s
--- sunphase_flask.py
+++ sunphase_flask.py
@@ -1,12 +1,14 @@
 from flask import Flask
 
 # create a flask object
+import sunphase_astral
+
 app = Flask(__name__)
 
 
-@app.route('/sunrise')
-def sunphase():
-    return 'Hello!!'
+@app.route('/sunrise/<name>')   # read the name(nearly equal to 'import')
+def sunphase(name):
+    return sunphase_astral.city_astral(name)
 
 
 if __name__ == '__main__':

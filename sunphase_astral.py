a45d04154bd7d6d16681b95f7f34dcacfe8f8471
astatim <astatim@outlook.com>
2022-08-09 13:42:45 +0800

    project sunphase: add input error check and change the output to the time of sunrise

--- sunphase_astral.py
+++ sunphase_astral.py
@@ -4,13 +4,16 @@ from astral.sun import sun
 
 
 def city_astral(name):
-
     # reformat the name
     name = name.strip().capitalize()
 
     # put the tuple into the dictionary
     LocationInfos = {'Beijing': ("Beijing", "China", "Asia/Shanghai", 39.904211, 116.407395),
                      'Shanghai': ("Shanghai", "China", "Asia/Shanghai", 31.231707, 121.472641)}
+
+    # input error check
+    if name not in LocationInfos:
+        return 'city not found'
 
     # read the tuple from the dictionary and divide it
     name, region, timezone, latitude, longitude = LocationInfos[name]
@@ -19,6 +22,6 @@ def city_astral(name):
     city = LocationInfo(name, region, timezone, latitude, longitude)
 
     # this will print the sunrise time
-    s = sun(city.observer, date=datetime.date(2022, 8, 7), tzinfo=timezone)
+    s = sun(city.observer, date=datetime.date(2022, 8, 9), tzinfo=timezone)
 
-    return s
+    return f'{s["sunrise"]} {timezone} {name}'
